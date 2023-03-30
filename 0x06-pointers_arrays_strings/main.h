f3685a737da639c4ea49a405d5eadaebea45ee1d
asoud80 <amgad_a.soud@outlook.com>
2023-03-30 08:52:20 -0700

    strcat

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,8 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-void print_rev(char *s);
-char *leet(char *);
 int _putchar (char c);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
