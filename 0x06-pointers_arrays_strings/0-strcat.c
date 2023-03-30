dc68b7f937298a1cf1708e691d633d4e677bf4f6
asoud80 <amgad_a.soud@outlook.com>
2023-03-30 08:27:19 -0700

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -7,6 +7,7 @@
  * Return: Always 0.
  */
 int main(void)
+
 {
 	char s1[98] = "Hello ";
 	char s2[] = "World!\n";
@@ -18,5 +19,6 @@ int main(void)
 	printf("%s", s1);
 	printf("%s", s2);
 	printf("%s", ptr);
+	
 	return (0);
 }
