a008120c4e7f1db26d9c95b544a4f44755cc9ea2
asoud80 <amgad_a.soud@outlook.com>
2023-11-25 10:09:12 -0800

    2-execute_a_command.pp

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,10 +1,10 @@
 # install flask version 2.1.0
 package { 'flask':
-	ensure => '2.1.0',
-	provider => 'pip3',
+ensure => '2.1.0',
+provider => 'pip3',
 }
 # install werkzeug version 2.0.0
 package {'werkzeug':
-	ensure => '0.16.1',
-	provider => 'pip3',
+ensure => '0.16.1',
+provider => 'pip3',
 }
