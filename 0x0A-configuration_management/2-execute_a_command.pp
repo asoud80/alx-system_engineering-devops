90223fa59ca33eb18dfd0b6be0a2c6a2840bb5a6
asoud80 <amgad_a.soud@outlook.com>
2023-11-25 10:08:04 -0800

    2-execute_a_command.pp

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,4 +1,10 @@
-#Kills a process named killmenow
-exec{'pkill -f killmenow':
-path  => '/usr/bin/:/usr/local/bin/:/bin/'
+# install flask version 2.1.0
+package { 'flask':
+	ensure => '2.1.0',
+	provider => 'pip3',
+}
+# install werkzeug version 2.0.0
+package {'werkzeug':
+	ensure => '0.16.1',
+	provider => 'pip3',
 }
