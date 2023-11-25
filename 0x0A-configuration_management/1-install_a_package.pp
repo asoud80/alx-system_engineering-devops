920eb1a8b2645af5ba89a8b84fab45a4cbd5e26d
asoud80 <amgad_a.soud@outlook.com>
2023-11-25 10:24:49 -0800

    2-execute_a_command.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,10 +1,5 @@
-# install flask versio 2.1.0
-package { 'falsk':
-	ensure => '2.1.0',
-	provider => 'pip3',
-}
-#install werkzeug version 0.16.1
-package {'werkzeug':
-	ensure => '0.16.1',
-	provider => 'pip3',
-}
+# install puppet-lint -v 2.5.0
+
+ exec { 'puppet-lint':
+   command => '/usr/bin/apt-get -y install puppet-lint -v 2.5.0',
+   }
