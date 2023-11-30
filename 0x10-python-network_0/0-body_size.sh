fa81096557e8ce4beb6885b4e37a24b730cac622
asoud80 <amgad_a.soud@outlook.com>
2023-11-30 15:25:03 -0800

    0-body_size.sh

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
-#!/bin/bash
+#!/usr/bin/python3
 # takes in a URL, sends a request to that URL, displays the size of the body of the response
 curl -sI GET "$1" | grep -i "Content-Length" | cut -d " " -f2
