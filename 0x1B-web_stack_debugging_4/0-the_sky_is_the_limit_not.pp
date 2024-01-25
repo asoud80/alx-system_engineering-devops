487f7e7c0b59dad64c82c3b6b37bd22c45bc48b8
asoud80 <amgad_a.soud@outlook.com>
2024-01-25 04:52:12 -0800

    0-the_sky_is_the_limit_not.pp

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -2,12 +2,16 @@
 
 # Increase the ULIMIT of the default file
 exec { 'fix--for-nginx':
+  #modify the ULIMIT value 
   command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  #specify the path for the sed command
   path    => '/usr/local/bin/:/bin/'
 } ->
 
 # Restart Nginx
 exec { 'nginx-restart':
+  # Restat Nginx service
   command => 'nginx restart',
+  #specify the path for the init.d scripts
   path    => '/etc/init.d/'
 }
